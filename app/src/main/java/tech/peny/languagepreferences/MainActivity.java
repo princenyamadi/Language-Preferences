9c12f02913f86d0f58609a7edf6fcdaf48afdcaa
princenyamadi <princenyamadi.pn@gmail.com>
2020-12-04 16:14:34 +0000

    [feat]: Added menu item

--- app/src/main/java/tech/peny/languagepreferences/MainActivity.java
+++ app/src/main/java/tech/peny/languagepreferences/MainActivity.java
@@ -1,5 +1,6 @@
 package tech.peny.languagepreferences;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,43 +8,86 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    SharedPreferences sharedPreferences;
+    TextView textView;
     public void setLanguage(String language){
-        SharedPreferences sharedPreferences = this.getSharedPreferences("tech.peny.languagepreferences", Context.MODE_PRIVATE);
-        sharedPreferences.edit().putString("language",language);
 
-        TextView textView= findViewById(R.id.textView);
+        sharedPreferences.edit().putString("language",language).apply();
+
+
 
         textView.setText(language);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()){
+            case R.id.english:
+                setLanguage("English");
+                return true;
+            case R.id.spanish:
+                setLanguage("Spanish");
+                return true;
+            default:
+                return false;
+
+
+        }
+
+
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//
+        sharedPreferences = this.getSharedPreferences("tech.peny.languagepreferences", Context.MODE_PRIVATE);
+        textView= findViewById(R.id.textView);
 
-        new AlertDialog.Builder(this)
-                .setIcon(android.R.drawable.ic_btn_speak_now)
-                .setTitle("Choose a language")
-                .setMessage("Which language would you like use?")
-                .setPositiveButton("English", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
+        String language = sharedPreferences.getString("language","Error");
+        if(language.equals("Error")){
+            new AlertDialog.Builder(this)
+                    .setIcon(android.R.drawable.ic_btn_speak_now)
+                    .setTitle("Choose a language")
+                    .setMessage("Which language would you like use?")
+                    .setPositiveButton("English", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
 //                        Set English
-                        setLanguage("English");
+                            setLanguage("English");
 
-                    }
-                })
-                .setNegativeButton("Spanish", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
+                        }
+                    })
+                    .setNegativeButton("Spanish", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
 //                        set French
-                        setLanguage("Spanish");
-                    }
-                })
-                .show();
+                            setLanguage("Spanish");
+                        }
+                    })
+                    .show();
+        }else{
+            textView.setText(language);
+
+        }
+//
+
     }
 }
